4f173691525c324a02d2f4dbd0e4a37c8309e827
Zafarzhon Irismetov <Zafarzhon.Irismetov@netent.com>
2021-11-30 08:55:31 +0200

    API developing

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
+import sqlite3
+from dataclasses import dataclass
+from dataclasses_json import dataclass_json
 from flask import Flask, render_template, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///HighScoreDatabase.sqlite?check_same_thread=False'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-class ScoreRecords(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    player_name = db.Column(db.String(20), nullable= False)
-    score = db.Column(db.Integer, nullable= False)
-    date_created = db.Column(db.DateTime, nullable=True, default = datetime.utcnow)
-
-    def __repr__(self):
-        return '<ScoreRecords %r>' % self.id
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///HighScoreDatabase.sqlite?check_same_thread=False'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#
+# db = SQLAlchemy(app)
+#
+# class ScoreRecords(db.Model):
+#     id = db.Column(db.Integer, primary_key = True)
+#     player_name = db.Column(db.String(20), nullable= False)
+#     score = db.Column(db.Integer, nullable= False)
+#
+#     def __repr__(self):
+#         return '<ScoreRecords %r>' % self.id
+#
+def create_table():
+    conn = sqlite3.connect('HighScoreDatabase.sqlite')
+    conn.execute('''CREATE TABLE score_records
+             (id INT PRIMARY KEY AUTOINCREMENT NOT NULL,
+             player_name           TEXT NOT NULL,
+             score            INT NOT NULL);''')
 
 
 #Cotroller
@@ -23,26 +30,26 @@ class ScoreRecords(db.Model):
 def index():
     if request.method == 'POST':
         try:
-            data = request.get_json()
-            if data_not_exist(data):
-                add_data(data)
-            rec = get_records()
-            jrec = get_json_view(rec)
-            return jrec
+            player = request.get_json()
+            if data_not_exist(player):
+                insert_highscore(player)
+            # rec = get_highscores()
+            # jrec = get_json_view(rec)
+            # return jrec
         except Exception as e:
             print('Data NOT insterted to DB! {}'.format(e))
     else:
-        return render_template('JS_7_basics_space_invaders_Zafarzhon_Irismetov.html')
+        return 'Hello worlds'
 
-@app.route('/get_rec', methods=['GET'])
+@app.route('/get_records', methods=['GET'])
 def get_rec():
-    rec = get_records()
+    rec = get_highscores()
     jrec = get_json_view(rec)
     return jrec
 
 
 #Model
-def add_data(data):
+def insert_highscore(data):
     player_name = list(data)[0]
     score = data.get(player_name)
     record = ScoreRecords(player_name=player_name, score=score)
@@ -53,7 +60,7 @@ def add_data(data):
 def data_not_exist(data):
     name = list(data)[0]
     score = data.get(name)
-    all_records = get_records()
+    all_records = get_highscores()
     for i in all_records:
         if name == i.player_name:
             if i.score < score:
@@ -66,7 +73,7 @@ def data_not_exist(data):
                 return False
     return True
 
-def get_records():
+def get_highscores():
     return ScoreRecords.query.order_by(ScoreRecords.score.desc()).limit(15).all()
 
 
